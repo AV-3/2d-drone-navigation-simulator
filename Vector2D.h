84a4e6d831bff4feaeaf3da2af523c41e8bd10e1
Alan <avele86@wgu.edu>
2025-07-10 23:51:17 -0500

    init file setup

---
+++ Vector2D.h
@@ -0,0 +1,9 @@
+#ifndef VECTOR2D_H
+#define VECTOR2D_H
+
+struct Vector2D {
+    double x = 0.0;
+    double y = 0.0;
+};
+
+#endif // VECTOR2D_H
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+
+int main() {
+    std::cout << "Drone Navigation Simulator Starting..." << std::endl;
+    return 0;
+}
